c274f15c7045d8f365558bdfff68880affe8b2f3
pantchayan <pantchayan@gmail.com>
2020-12-03 22:48:01 +0530

    index script

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+// CORE MODULES
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
 
-console.log(fs);
+const tempOverview = fs.readFileSync(`${__dirname}/templates/overview-template.html`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/card-template.html`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/product-template.html`,'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
+const dataObj = JSON.parse(data);
+
+
+
