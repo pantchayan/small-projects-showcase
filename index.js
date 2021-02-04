47b6ebe84d90829d306ececeed19df2ffbfc7377
pantchayan <pantchayan@gmail.com>
2021-02-04 13:22:45 +0530

    updates in Index

--- index.js
+++ index.js
@@ -1,75 +1,80 @@
 // CORE MODULES
-const fs = require('fs');
-const http = require('http');
-const url = require('url');
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
+const replaceTemplate = (template, el) => {
+  let output = template.replace(/{%PROJECTNAME%}/, el.projectName);
+  output = output.replace(/{%ID%}/, el.id);
+  output = output.replace(/{%TECH_STACK%}/, el.stack);
+  output = output.replace(/{%GITHUB%}/, el.github);
+  output = output.replace(/{%DEMO%}/, el.demo);
+  output = output.replace(/{%ICON%}/, el.icon);
+  output = output.replace(/{%IMAGE%}/, el.image);
+  output = output.replace(/{%DESCRIPTION%}/, el.description);
 
-const replaceTemplate = (template, el) =>{
-    let output = template.replace(/{%PROJECTNAME%}/, el.projectName);
-    output = output.replace(/{%ID%}/,el.id);
-    output = output.replace(/{%TECH_STACK%}/, el.stack);
-    output = output.replace(/{%GITHUB%}/,el.github);
-    output = output.replace(/{%DEMO%}/,el.demo);
-    output = output.replace(/{%ICON%}/, el.icon);
-    output = output.replace(/{%IMAGE%}/,el.image);
-    output = output.replace(/{%DESCRIPTION%}/, el.description);
-
-    if(!el.dynamic){
-        output = output.replace(/{%NOT_DYNAMIC%}/, "not-dynamic");
-    }
-    return output;
+  if (!el.dynamic) {
+    output = output.replace(/{%NOT_DYNAMIC%}/, "not-dynamic");
+  }
+  return output;
 };
 
-const tempOverview = fs.readFileSync(`${__dirname}/templates/overview-template.html`,'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/card-template.html`,'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/product-template.html`,'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/overview-template.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/card-template.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/product-template.html`,
+  "utf-8"
+);
 
-
-
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+// READING DATABASE ==
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 
 const dataObj = JSON.parse(data);
 
-
-
 const server = http.createServer((req, res) => {
-    const {query, pathname} = url.parse(req.url,true);
+  const { query, pathname } = url.parse(req.url, true);
 
-    //OVERVIEW
-    if (pathname === "/" || pathname === "/overview") {
-        res.writeHead(200,{"Content_type": "text/html"});
-        
-        const cardsHTML = dataObj.map(el=>{
-            return replaceTemplate(tempCard,el);
-        }).join("");
+  //OVERVIEW
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { Content_type: "text/html" });
 
+    const cardsHTML = dataObj
+      .map((el) => {
+        return replaceTemplate(tempCard, el);
+      })
+      .join("");
 
-        const output = tempOverview.replace("{%PROJECTCARDS%}",cardsHTML); 
+    const output = tempOverview.replace("{%PROJECTCARDS%}", cardsHTML);
 
-        res.end(output);
-    }
-    //PRODUCT
-    else if (pathname === "/project") {
-        res.writeHead(200,{"Content_type": "text/html"});
+    res.end(output);
+  }
+  //PRODUCT
+  else if (pathname === "/project") {
+    res.writeHead(200, { Content_type: "text/html" });
 
-        const product = dataObj[query.id];
-        const output = replaceTemplate(tempProduct,product);
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
 
-        res.end(output);
-    }
-    //API
-    else if (pathname === "/api") {
-
-        res.writeHead(200, { "Content_type": "application/json" });
-        res.end(data);
-    }
-    //NOT FOUND
-    else {
-        res.writeHead(404);
-        res.end(`<h1>Page not found</h1>`);
-    }
+    res.end(output);
+  }
+  //API
+  else if (pathname === "/api") {
+    res.writeHead(200, { Content_type: "application/json" });
+    res.end(data);
+  }
+  //NOT FOUND
+  else {
+    res.writeHead(404);
+    res.end(`<h1>Page not found</h1>`);
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
-    console.log("Listening to requests on port 8000");
+  console.log("Listening to requests on port 8000");
 });
